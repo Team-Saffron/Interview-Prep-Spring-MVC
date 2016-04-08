6dd1dbb59af4d1656ff081b431673b32f868612d
archit09jain <archit08jain@gmail.com>
2016-04-08 14:40:26 -0700

    basic GUI

---
+++ src/java/controllers/Dashboard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author archit08jain
+ */
+@Controller
+@RequestMapping("/dashboard.htm")
+public class Dashboard {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String getDashboard(ModelMap modelMap) {
+     //   System.out.println("on method");
+       // modelMap.put("printme", "Hello Spring !!");
+        return "dashboard";
+    }
+}
